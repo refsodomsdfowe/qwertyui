376a08616219834c1cf7d8337492a99e33530e69
refsodomsdfowe <refsodomsdfowei@proton.me>
2026-06-12 09:46:31 -0400

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fd696659b928dd53"; }
+/******/ 		__webpack_require__.h = function() { return "4258e78d0b1dc744"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
